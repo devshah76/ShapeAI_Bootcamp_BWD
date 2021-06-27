28fed9a671fb8b46706ea0ed73cd8f7c1859a516
devshah76 <86524428+devshah76@users.noreply.github.com>
2021-06-27 11:58:18 +0530

    Add files via upload

---
+++ Info1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info1() {
+  return (
+    <div className="note">
+      <h1>JavaScript and Nodejs</h1>
+      <p>Foundation of Javascript and Backend with NodeJS</p>
+    </div>
+  );
+}
+export default Info1;
